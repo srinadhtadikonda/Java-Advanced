20f35b8cf69b1d1e36255aee6292fb901ba654d8
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2023-05-03 13:08:23 +0530

    Update Test01 (29).java

--- Test01 (29).java
+++ Test01 (29).java
@@ -1,9 +1,26 @@
-import java.sql.*;
-class Test29
- {
-public static void main(String[] args) 
+mysql> create table users(id int,name char(20),email char(30),country char(3),password char(10));
+
+mysql>insert into users values(1,'raj','raj@gmail.com','ind','abc123');
+mysql>insert into users values(2,'madhu','madhu@gmail.com','sri','ab123a');
+
+DELIMITER $$
+USE `demobase`$$
+CREATE PROCEDURE `retreive_users` ()
+BEGIN
+ select * from users;
+END$$
+DELIMITER ;
+
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+class Test 
 {
-String jdbcUrl = "jdbc:mysql://localhost:3306/demobase";
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/demobase?useSSL=false";
         String username = "root";
         String password = "brilliant";
         String sql = "call retreive_users()";
@@ -20,3 +37,4 @@ String jdbcUrl = "jdbc:mysql://localhost:3306/demobase";
         }
     }
 }
+

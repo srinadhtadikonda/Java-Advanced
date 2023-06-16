15646fa65d9a66dc8913a94a337c4e9c76509c1b
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2023-06-16 17:15:35 +0530

    Update prepared Statement.java

--- jdbc/prepared Statement.java
+++ jdbc/prepared Statement.java
@@ -1,45 +1,44 @@
+//program to insert record using Prepared Statement
+
 import java.io.*;
 import java.sql.*;
 
-public class Irs{
-	public static void main(String[] args) {
-		System.out.println("Insert records example using prepared statement!");
-		Connection con = null;
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demobase","root","brilliant");
-			try{
-				String sql = "INSERT into myemp VALUES(?,?,?,?,?)";
-				PreparedStatement prest = con.prepareStatement(sql);
-				BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("Enter eno:");
-				int eno = Integer.parseInt(bf.readLine());
-				prest.setInt(1, en);
-				System.out.println("Enter ename");
-				String ename=bf.readLine();
-				prest.setString(2, ename);
-
-				System.out.println("Enter esal");
-				int esal = Integer.parseInt(bf.readLine());
-				prest.setInt(3, esal);	
-				System.out.println("Enter egrade");
-				String egrade=bf.readLine();
-				prest.setString(4,egrade);
-				
-				System.out.println("Enter epf");
-				int epf = Integer.parseInt(bf.readLine());
-				prest.setInt(5, epf);	
-				
-				int count = prest.executeUpdate();
-				System.out.println(count + "row(s) affected");
-				con.close();
-			}
-			catch (SQLException s){
-				System.out.println("SQL statement is not executed!");
-			}
-		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
-	}
+public class Irs
+{
+public static void main(String[] args) 
+{
+System.out.println("Insert records example using prepared statement!");
+Connection con = null;
+try
+{
+Class.forName("com.mysql.jdbc.Driver");
+con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demobase","root","brilliant");
+try{
+String sql = "INSERT into myemp VALUES(?,?,?,?)";
+PreparedStatement prest = con.prepareStatement(sql);
+BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter eno:");
+int eno = Integer.parseInt(bf.readLine());
+prest.setInt(1, eno);
+System.out.println("Enter ename");
+String ename=bf.readLine();
+prest.setString(2, ename);
+System.out.println("Enter esal");
+int esal = Integer.parseInt(bf.readLine());
+prest.setInt(3, esal);	
+System.out.println("Enter egrade");
+String egrade=bf.readLine();
+prest.setString(4,egrade);
+int count = prest.executeUpdate();
+System.out.println(count + "row(s) affected");
+con.close();
+}
+catch (SQLException s){
+System.out.println(s);
+}
+}
+catch (Exception e){
+e.printStackTrace();
+}
+}
 }

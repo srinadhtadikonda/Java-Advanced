8c8dbeb5f27b8b619d9c62b300052e7f6afe0a0e
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2023-06-30 16:07:25 +0530

    Add files via upload

---
+++ Collections/ArrayListExample6.java
@@ -0,0 +1,24 @@
+// Java  program to iterate over an ArrayList
+// Using for loop
+ 
+// Importing all utility classes
+import java.util.*;
+ 
+// Main class
+class GFG {
+   
+    // Main driver method 
+    public static void main(String[] args)
+    {
+        // Creating and initializing the ArrayList
+        // Declaring object of integer type
+        List<Integer> numbers = Arrays.asList(1, 2, 3,
+                                       4, 5, 6, 7, 8);
+ 
+        // Iterating using for loop
+        for (int i = 0; i < numbers.size(); i++)
+           
+            // Printing and display the elements in ArrayList
+            System.out.print(numbers.get(i) + " ");       
+    }
+}

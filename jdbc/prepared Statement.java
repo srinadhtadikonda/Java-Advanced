026f19a3a532186e29b91a8f2231d5edc0b3f192
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2023-07-06 15:30:09 +0530

    Update prepared Statement.java

--- jdbc/prepared Statement.java
+++ jdbc/prepared Statement.java
@@ -17,27 +17,29 @@ try{
 String sql = "INSERT into myemp VALUES(?,?,?,?)";
 PreparedStatement prest = con.prepareStatement(sql);
 BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-System.out.println("Enter eno:");
+System.out.print("Enter eno...:");
 int eno = Integer.parseInt(bf.readLine());
 prest.setInt(1, eno);
-System.out.println("Enter ename");
+System.out.print("Enter ename...:");
 String ename=bf.readLine();
 prest.setString(2, ename);
-System.out.println("Enter esal");
+System.out.print("Enter esal...:");
 int esal = Integer.parseInt(bf.readLine());
 prest.setInt(3, esal);	
-System.out.println("Enter egrade");
+System.out.print("Enter egrade...:");
 String egrade=bf.readLine();
 prest.setString(4,egrade);
 int count = prest.executeUpdate();
-System.out.println(count + "row(s) affected");
+System.out.println(count +" "+"row(s) affected");
 con.close();
 }
-catch (SQLException s){
+catch (SQLException s)
+{
 System.out.println(s);
 }
 }
-catch (Exception e){
+catch (Exception e)
+{
 e.printStackTrace();
 }
 }

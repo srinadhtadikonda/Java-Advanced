55915d684eba21c508e0d0c3e17d8f88ce937800
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2023-10-21 18:01:41 +0530

    Create ArrayListExample04.java

---
+++ Collections/ArrayListExample04.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ArrayListReverseDemo {
+
+    public static void main(String args[]) {
+
+        ArrayList<String> listOfInts = new ArrayList<>();
+        listOfInts.add("1");
+        listOfInts.add("2");
+        listOfInts.add("3");
+        listOfInts.add("4");
+        listOfInts.add("5");
+        
+        System.out.println("Before Reversing : " + listOfInts);
+        Collections.reverse(listOfInts);
+        System.out.println("After Reversing : " + listOfInts);
+       
+
+    }
+
+}

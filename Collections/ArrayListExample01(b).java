7eedf672d6ddbcc50c3c268bcd31926b240d2645
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-08-22 09:37:24 +0530

    Create ArrayListExample01(b).java

---
+++ Collections/ArrayListExample01(b).java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayListExample1
+{
+public static void main(String args[])
+{
+ArrayList<Integer>AT=new ArrayList<Integer>();
+System.out.println(AT);
+System.out.println(AT.size());
+AT.add(10);
+AT.add(20);
+AT.add(30);
+System.out.println(AT);
+}
+}

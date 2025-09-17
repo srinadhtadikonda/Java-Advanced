56efc3e484948347bd272cff74febf315750df2c
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-09-17 14:37:17 +0530

    Update ArrayDequeExample.java

--- Collections/ArrayDequeExample.java
+++ Collections/ArrayDequeExample.java
@@ -3,7 +3,7 @@ class Test17
  {
 public static void main(String[] args) 
 {
-ArrayDeque queue = new ArrayDeque();
+ArrayDeque<String> queue = new ArrayDeque<String>();
 queue.add("Java");
 queue.add(".NET");
 queue.add("Javascript");

b0a4801f90e9578378b5d735e21f3b142dfb1e80
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-12-13 16:53:21 +0530

    Update PriorityQueueExample.java

--- Collections/PriorityQueueExample.java
+++ Collections/PriorityQueueExample.java
@@ -1,14 +1,19 @@
-import java.util.*;
-class Main
- {
-public static void main(String[] args) 
-{
-PriorityQueue<String>queue=new PriorityQueue<String>();
-queue.add("JAVA");
-queue.add("NET");
-queue.add("JULIA");
-queue.add("ADA");
-queue.add("HTML5");
-System.out.println(queue);
-}
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(30);
+        pq.add(10);
+        pq.add(20);
+        pq.add(40);
+
+        System.out.println("Priority Queue: " + pq);
+
+        System.out.println("Peek: " + pq.peek());
+        }
+    }
 }

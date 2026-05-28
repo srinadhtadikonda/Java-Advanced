12ef8fcc88c6fc2982e28a3e13c91febd0faf6d1
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-05-28 09:50:33 +0530

    Update DequeExample02.java

--- Collections/DequeExample02.java
+++ Collections/DequeExample02.java
@@ -1,66 +1,19 @@
-Deque as a Queue (FIFO)
+//Using Deque as stack
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class DequeQueueExample {
-
+public class StackUsingDeque {
     public static void main(String[] args) {
 
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        dq.offerLast(10);
-        dq.offerLast(20);
-        dq.offerLast(30);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
 
-        System.out.println("Queue: " + dq);
-
-        System.out.println("Removed: " + dq.pollFirst());
-        System.out.println("Front: " + dq.peekFirst());
-    }
-}
-
-Output
-Queue: [10, 20, 30]
-Removed: 10
-Front: 20
-
-✅ Deque as a Stack (LIFO)
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-public class DequeStackExample {
-
-    public static void main(String[] args) {
-
-        Deque<String> stack = new ArrayDeque<>();
-
-        stack.push("Java");
-        stack.push("Python");
-        stack.push("C");
-
-        System.out.println("Stack: " + stack);
+        System.out.println(stack);
 
         System.out.println("Pop: " + stack.pop());
         System.out.println("Peek: " + stack.peek());
     }
 }
-
-Output
-Stack: [C, Python, Java]
-Pop: C
-Peek: Python
-
-✅ Add / Remove from Both Ends
-Deque<Integer> dq = new ArrayDeque<>();
-
-dq.addFirst(1);
-dq.addLast(2);
-dq.addFirst(0);
-dq.addLast(3);
-
-System.out.println(dq);   // [0, 1, 2, 3]
-
-dq.removeFirst();         // removes 0
-dq.removeLast();          // removes 3
-
-System.out.println(dq);   // [1, 2]
